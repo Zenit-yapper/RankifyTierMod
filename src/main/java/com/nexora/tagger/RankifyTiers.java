20d91f440b5ec8c8fc58943714113af8998f7a31
Zenit-yapper <zenitog69@gmail.com>
2026-03-22 12:35:08 +0530

    RankifyTiers.java

---
+++ src/main/java/com/nexora/tagger/RankifyTiers.java
@@ -0,0 +1,9 @@
+package com.nexora.tagger;
+import net.fabricmc.api.ClientModInitializer;
+
+public class RankifyTiers implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+        System.out.println("Rankify Mod Loaded!");
+    }
+}
